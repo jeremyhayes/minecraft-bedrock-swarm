2554b73d39cb1e2a407262c2c0f2f459d10375f1
Jeremy Hayes <jeremyhayes@users.noreply.github.com>
2022-09-10 02:58:22 +0000

    add offline backup service

---
+++ offline-backup.sh
@@ -0,0 +1,22 @@
+#! /bin/sh
+
+#SERVER_DIR=/opt/apps/minecraft/server
+SERVER_DIR=${SERVER_DIR:?required}
+
+#BACKUP_DIR=/opt/apps/minecraft/backups
+BACKUP_DIR=${BACKUP_DIR:?required}
+
+DATE_FMT="%Y-%m-%d_%H-%M-%S"
+
+echo "Stopping bedrock service..."
+docker service scale mc_bedrock=0
+
+for world_dir in $SERVER_DIR/worlds/*; do
+  world_name=$(basename "$world_dir")
+  target="$BACKUP_DIR/$world_name-$(date +$DATE_FMT).tar.gz"
+  echo "Backing up $world_name to $target..."
+  tar -cvpzf "$target" "$world_dir"
+done
+
+echo "Starting bedrock service..."
+docker service scale mc_bedrock=1
